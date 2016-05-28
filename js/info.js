a1da8f2cd6479c891b972a8102b69b76746db54c
aminawholegrain <amina@wholegraindigital.com>
2016-05-28 11:14:16 +0100

    updated site

---
+++ js/info.js
@@ -0,0 +1,67 @@
+(function() {
+  var appendContent = function() {
+    var text = '<p>The brain is the center of the human nervous system, controlling our thoughts, movements, memories and decisions.</p>',
+        contentDiv = document.querySelector('.content'),
+        card = document.querySelector('#loadingCard');
+    
+    var letShow = function() {
+          TweenMax.to(contentDiv, .3,
+          {
+            x: 50,
+            rotation: '5deg'
+            
+          })},
+        letHide = function() {
+      TweenMax.to(contentDiv, .3,
+      {
+        x: 0,
+        rotation: '0deg'
+      })
+    };
+                  
+    
+    card.addEventListener('mouseover', letShow)
+    card.addEventListener('mouseleave', letHide)
+    
+    contentDiv.innerHTML = text;
+    contentDiv.offsetWidth = loadingWidth;
+    
+    card.addEventListener('click', function() {
+      var contentAnimation = new TimelineMax();
+      this.removeEventListener('mouseover', letShow);
+      letHide();
+      contentAnimation.add(TweenMax.to(card, 1.2, {
+                  width: loadingWidth*2,
+        ease: Elastic.easeOut
+                }));
+      
+      console.log( loadingWidth );
+      
+      
+      
+    })
+    
+    
+    
+  }
+  
+  
+  var animation = new TimelineMax(),
+      loadingWidth = document.querySelector('#loadingCard').offsetWidth;
+  
+  animation.add(TweenMax.to('.cover', 3, {
+                  width: loadingWidth
+                }));
+  animation.add(TweenMax.to('#loadingCard', 1, {
+                  height: 384,
+                  ease: Elastic.easeOut,
+                  cursor: 'pointer'
+                }));
+  animation.add(TweenMax.staggerTo('.stagger', .7, {
+                  opacity: 1,
+                  display: 'block',
+                  delay: -1,
+                }, 0.2, appendContent));
+
+
+}())
